788c6de6288a31259af716c7e240f3e0609be4e4
tvarderesyan <colovarderesyan@yahoo.com>
2018-04-10 00:11:07 +0300

    2

--- src/main/java/task4_massiv2.java
+++ src/main/java/task4_massiv2.java
@@ -6,39 +6,52 @@ import java.util.Scanner;
 public class task4_massiv2 {
 
 
-        public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            int[] array;
-            array = new int[20];
-            int max = -999999;
-            int min = 999999;
-            System.out.println("Created massiv:");
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] array;
+        array = new int[20];
+        int max = -999999;
+        int min = 999999;
+        int nMax = array[0];
+        int nMin = array[0];
+        System.out.println("Created massiv:");
 // Создание масива
-            for (int i = 0; i < array.length; i++) {
-                array[i] = ((int) (Math.random() * 20) - 10);
-                System.out.print(array[i] + " , ");
-            }
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int) (Math.random() * 20) - 10);
+            System.out.print(array[i] + " , ");
+        }
 // Поиск максимального отрицательного элемента массива
-            for (int el : array) {
-                if (el < 0 && el > max)
-                    max = el;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0 && array[i] > max) {
+                max = array[i];
+                nMax = i;
             }
+        }
+
 // Поиск минимального положительного элементы массива
-            for (int el : array) {
-                if (el > 0 && el < min)
-                    min = el;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0 && array[i] < min) {
+                min = array[i];
+                nMin = i;
             }
+        }
 
-            System.out.println("\n maximum negative of massiv is: " + max);
-            System.out.println(" minimum positive of massiv is: " + min);
+        System.out.println("\n maximum negative of massiv is: " + max);
+        System.out.println(" minimum positive of massiv is: " + min);
 // Меняем местами максимальный отрицательный и минимальный положительный элементов массива
-            System.out.print("\nLets change places of maximum negativ and minimum positive: ");
-            int d = max;
-            max = min;
-            min = d;
+        System.out.print("\nLets change places of maximum negativ and minimum positive: ");
 
-            System.out.println("\nmaximum: " + max);
-            System.out.println("minimum: " + min);
+        int tmp = array[nMax];
+        array[nMax] = array[nMin];
+        array[nMin] = tmp;
+        for (int item : array) {
+            System.out.print(item + " , ");
+
         }
     }
+}
+
+
 
